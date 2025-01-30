44812b0e050a6ed9deff206d93faa8f41e5c16b5
Dayve08 <163396571+Dayve08@users.noreply.github.com>
2025-01-30 13:02:17 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_ZqqOg5Lmx', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3kpvhh4rso7s5rahvhrj8qsr39', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '63bt6knt33gegc1muh0g4k0cve', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
