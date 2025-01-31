01b8dde0927259d04b27cb28d7502a540463f918
Dayve08 <163396571+Dayve08@users.noreply.github.com>
2025-01-31 13:23:54 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ window._config = {
         userPoolId: 'us-east-1_A1Ncxh7CS', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '440hajp48bgpb8d8e2nom69uuc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
-        secret_hash: 'fo0jjo78iu4bba1rd0h9ivipaa8k3fproural26dm7koq0me0d8'
+        SECRET_HASH: 'fo0jjo78iu4bba1rd0h9ivipaa8k3fproural26dm7koq0me0d8'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
