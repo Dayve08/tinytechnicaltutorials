9813038df81a09b32a4b9bc1884c38e084c64097
Dayve08 <163396571+Dayve08@users.noreply.github.com>
2025-01-31 13:27:19 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_A1Ncxh7CS', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '440hajp48bgpb8d8e2nom69uuc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        region: 'us-east-1', // e.g. us-east-2
         SECRET_HASH: 'fo0jjo78iu4bba1rd0h9ivipaa8k3fproural26dm7koq0me0d8'
     },
     api: {
